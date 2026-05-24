ab5309bb26a2d913f3a6a735dbee46dfc793e6e3
Twor <tlansantad@gmail.com>
2026-05-24 19:19:26 +0700

    add thai font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,14 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.svg') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans+Thai:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/fonts/boxicons.css') }}">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/css/core.css') }}" class="template-customizer-core-css">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/css/demo.css') }}">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom-overrides.css') }}">
+    <style>body{font-family:'Public Sans','Noto Sans Thai',sans-serif}:root{--bs-font-sans-serif:'Public Sans','Noto Sans Thai',sans-serif}</style>
     @yield('style')
     <script src="{{ asset('sneat-1.0.0/assets/vendor/js/helpers.js') }}"></script>
     <script src="{{ asset('sneat-1.0.0/assets/js/config.js') }}"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,14 @@
     <title>เข้าสู่ระบบ - ผู้ดูแลระบบ</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans+Thai:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/fonts/boxicons.css') }}">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/css/core.css') }}" class="template-customizer-core-css">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/css/demo.css') }}">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}">
     <link rel="stylesheet" href="{{ asset('sneat-1.0.0/assets/vendor/css/pages/page-auth.css') }}">
+    <style>body{font-family:'Public Sans','Noto Sans Thai',sans-serif}</style>
     @php $favicon = setting('favicon'); @endphp
     @if ($favicon)
         <link rel="icon" type="image/png" href="{{ Storage::disk('public')->url($favicon) }}">
